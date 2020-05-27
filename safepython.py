db6f40d06d86995887dff42363b4ec6060a76472
Juno Raihala <jcraihala@gmail.com>
2020-05-27 12:38:47 -0500

    Add hardcore mode

--- safepython.py
+++ safepython.py
@@ -28,7 +28,7 @@ def execfile(filename, globals=None, locals=None):
         exec(compile(file.read(), filename, 'exec'), globals, locals)
 
 
-def wrap(filename, lines_commented=0):
+def wrap(filename, hardcore=False, lines_commented=0):
     try:
         execfile(filename)
     except:
@@ -36,8 +36,12 @@ def wrap(filename, lines_commented=0):
         stack = traceback.extract_tb(tb)
 
         # frame 0 is execfile() call, frame 1 is exec() call,
-        # frame 2 is source of error in the wrapped file
-        frame = stack[2]
+        # frame 2 is source of error in the wrapped file, and
+        # the last frame (-1) is the ultimate source of the error
+        if hardcore:
+            frame = stack[-1]
+        else:
+            frame = stack[2]
 
         offending_filename = frame.filename
         offending_lineno = frame.lineno
@@ -45,7 +49,7 @@ def wrap(filename, lines_commented=0):
 
         # comment out the offending line and try again
         comment_line(offending_filename, offending_lineno)
-        wrap(filename, lines_commented+1)
+        wrap(filename, hardcore, lines_commented+1)
     else:
         print(f"Executed {filename} successfully after commenting out {lines_commented} lines!")
 
@@ -53,12 +57,16 @@ def wrap(filename, lines_commented=0):
 def main(argv=sys.argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help='Python file to wrap')
+    parser.add_argument(
+        '--hardcore', action='store_true',
+        help='Doggedly pursue exceptions to their ultimate source, and fix them there'
+    )
     args = parser.parse_args(argv[1:])
 
     # <<;;;
     shutil.copy2(args.filename, f"{args.filename}.bak")
 
-    wrap(args.filename)
+    wrap(args.filename, hardcore=args.hardcore)
 
 
 if __name__ == "__main__":
